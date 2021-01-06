c35de7d2e91e8953fd1a3425a8774fe975b407e8
dynamic-chain-5255 <ritiguda@gmail.com>
2021-01-06 20:56:04 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,18 +1,14 @@
 function basicTeenager(age)
 {
-  if(13<= age <= 19)
+  if(age >= 13 && age <= 19)
     {
       return "You are a teenager!"
-    }
-  else
-    {
-      return "You are not a teenager!"
     }
 }
 
 function teenager(age)
 {
-  if(13<= age <= 19)
+  if(age >= 13 && age <= 19)
     {
       return "You are a teenager!"
     }
@@ -24,11 +20,11 @@ function teenager(age)
 
 function ageChecker(age)
 {
-  if(13<= age <= 19)
+  if(age >= 13 && age <= 19)
     {
       return "you are a teenager!"
     }
-  else if (age <= 12)
+  else if (age < 13)
     {
       return "You are a kid!"
     }
@@ -39,9 +35,21 @@ function ageChecker(age)
 }
 
 function ternaryTeenager(age) {
-
+return age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
-
+  switch(age)
+  {
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+    case 19:
+      return "You are a teenager!";
+    default:
+      return "You have an age"
+  } 
 }
